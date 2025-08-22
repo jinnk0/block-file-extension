5ecf90b53938e2646a66df506ca051254fc2a465
jinnk0 <jin8073@gmail.com>
2025-08-22 12:16:36 +0900

    Initial commit

---
+++ src/test/java/com/example/blockfileextension/BlockfileextensionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blockfileextension;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockfileextensionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
