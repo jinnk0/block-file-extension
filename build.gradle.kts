222fadb81962bd525472ba98615e38900aaea449
jinnk0 <jin8073@gmail.com>
2025-08-24 15:23:18 +0900

    chore: Dockerfile 및 gradle 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,10 @@ java {
 	}
 }
 
+tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    archiveFileName.set("app.jar")
+}
+
 configurations {
 	compileOnly {
 		extendsFrom(configurations.annotationProcessor.get())
