a577ac7f8dad25e3e31327779d978b806e2599f5
jinnk0 <jin8073@gmail.com>
2025-08-28 15:50:49 +0900

    fix: 커스텀 확장자 추가 시 확장자 명 DB 테이블에서 unique로 처리 (동시성 고려)

--- src/main/java/com/example/blockfileextension/domain/BlockedFileExtension.java
+++ src/main/java/com/example/blockfileextension/domain/BlockedFileExtension.java
@@ -14,6 +14,7 @@ public class BlockedFileExtension {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String extension;
 
     @Enumerated(EnumType.STRING)
--- src/main/java/com/example/blockfileextension/domain/ExtensionFrequency.java
+++ src/main/java/com/example/blockfileextension/domain/ExtensionFrequency.java
@@ -1,9 +1,6 @@
 package com.example.blockfileextension.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +14,7 @@ public class ExtensionFrequency {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String extension;
 
     private long addedCount;
